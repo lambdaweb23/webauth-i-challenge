4b3894f5973876c7cf79175b26ce1d92f928dab8
Kenna Lawrie <kenna@kennas-mbp.home>
2019-11-11 16:15:29 -0800

    set up file structure, knex, server.js, index.js

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('experss');
+
+//const apiRouter = require('./api-router')
+const configureMiddleware = require('./configure-middleware')
+
+const server = express();
+
+configureMiddleware(server);
+
+//server.use('./api', apiRouter);
+
+
+module.exports = server;
