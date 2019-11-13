54739000a3a8b13158233b202781c76b0529ef36
Kenna Lawrie <kenna@kennas-mbp-2.home>
2019-11-12 16:11:23 -0800

    register and login working but get request isn't

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -2,24 +2,32 @@ const bcrypt = require('bcryptjs');
 
 const Users = require('../auth/auth-model');
 
-module.exports = (req, res, next) => {
-    let { username, password } = req.headers;
+// module.exports = (req, res, next) => {
+//     let { username, password } = req.headers;
 
-    if(username && password) {
-        Users.findBy({ username })
-        //first() takes it out of an array. An empty array is truthy so we can't do an if else for truthiness if it was an array (not doing here)
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(password, user.password)) {
-                next();
-            } else {
-                res.status(401).json({ message: 'Invalid Credentials' })
-            }
-        })
-        .catch(err => {
-            res.status(500).json({ error: 'Server Error' })
-        })
+//     if(username && password) {
+//         Users.findBy({ username })
+//         //first() takes it out of an array. An empty array is truthy so we can't do an if else for truthiness if it was an array (not doing here)
+//         .first()
+//         .then(user => {
+//             if(user && bcrypt.compareSync(password, user.password)) {
+//                 next();
+//             } else {
+//                 res.status(401).json({ message: 'Invalid Credentials' })
+//             }
+//         })
+//         .catch(err => {
+//             res.status(500).json({ error: 'Server Error' })
+//         })
+//     } else {
+//         res.status(400).json({ message: 'Please provide credentials' })
+//     }
+// }
+
+module.exports = (req, res, next) => {
+    if (req.session && req.session.user) {
+      next();
     } else {
-        res.status(400).json({ message: 'Please provide credentials' })
+      res.status(401).json({ message: "Not Authorized" });
     }
-}
+  };
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const configureMiddleware = require('./middleware/configure-middleware');
 const authRouter = require('./auth/auth-router.js');
+const session = require('express-session');
+
 const server = express();
+
+const sessionConfig = {
+    name: "booger", // default name is sid
+    secret: process.env.COOKIE_SECRET || "is it secret? is it safe?",
+    cookie: {
+      maxAge: 1000 * 60 * 60, // valid for 1 hour (in milliseconds)
+      secure: process.env.NODE_ENV === "development" ? false : true, // do we send cookie over https only?
+      httpOnly: true // prevent client javascript code from accessing the cookie
+    },
+    resave: false, // save sessions even when they have not changed
+    saveUninitialized: true, // read about it on the docs to respect GDPR
+    // store: new KnexSessionStorage({
+    //   knex: knexConnection,
+    //   clearInterval: 1000 * 60 * 10, // delete expired sessions every 10 minutes
+    //   tablename: "user_sessions",
+    //   sidfieldname: "id",
+    //   createtable: true
+    // })
+   };
 
 configureMiddleware(server);
 
 server.use(express.json());
+server.use(session(sessionConfig));
 server.use('/api/auth', authRouter);
 
 module.exports = server;
